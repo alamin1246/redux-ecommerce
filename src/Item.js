907d9cad18d3a9c2878ec9e868e2be75f3bd9bc1
unknown <Alamin>
2022-09-12 11:35:08 +0600

    searching product feature added

--- src/Item.js
+++ src/Item.js
@@ -5,12 +5,12 @@ export default function Item({ item }) {
   const dispatch = useDispatch();
 
   return (
-    <div className="shadow p-5 mb-5 bg-white rounded">
+    <div className="shadow p-5 mt-5 mb-3 bg-white rounded">
       <h1>Product Name: {item.itemName}</h1>
       <img src={item.itemImage} className="img-fluid" />
       <h1>Product Price: Tk.{item.itemPrice} /-</h1>
       <button
-        className="btn btn-primary"
+        className="btn btn-primary p-1"
         onClick={() => {
           dispatch({ type: "ADD_ITEM_TO_CART", payload: item });
         }}
--- src/Items.js
+++ src/Items.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import Item from "./Item";
 
 export default function Items() {
   const itemObj = useSelector((store) => store.itemsReducer);
-  const itemsList = itemObj.items;
+  // const [itemsList] = itemObj.items;
+  const [itemsList, setItemsList] = useState(itemObj.items);
+  const [searchKey, setSearchKey] = useState("");
 
+  function filterItems() {
+    // alert(searchKey);
+    const duplicateList = itemObj.items;
+    const filteredList = duplicateList.filter((item) =>
+      item.itemName.toLowerCase().includes(searchKey.toLowerCase())
+    );
+    setItemsList(filteredList);
+  }
   const itemsData = itemsList.map((item) => {
     return (
       <div className="col-md-4">
@@ -16,6 +26,16 @@ export default function Items() {
   return (
     <div className="container">
       <h1>Products List</h1>
+      <input
+        type="text"
+        className="form-control mb-5"
+        placeholder="Search Products"
+        value={searchKey}
+        onChange={(e) => {
+          setSearchKey(e.target.value);
+        }}
+        onKeyUp={filterItems}
+      />
       {/* <h1>Total Items = {itemObj.items.length}</h1> */}
       <div className="row">{itemsData}</div>
     </div>
